3b8a0b178ff5653515185b1b95fdd153d2b38474
hobotok85 <s_a_m_85@tut.by>
2022-08-14 12:55:44 +0300

    Created new project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+};
+const Field = () => {
+   const holder = "TypeHere"
+   const styleField = {
+      width: '300px'
+  };
+   return <input 
+      placeholder={holder} 
+      type="text" 
+      style={styleField}/>
+};  
+function Btn() {
+  // const text = 'Log in';
+   const res = () => {
+     return 'Log in'};
+  return <button>{res()}</button>
+};
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
